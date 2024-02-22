23ae273227ab46c3efcd6f160ce81f7dcd2c5f64
Austin Song <austinhssong@gmail.com>
2024-02-22 11:58:53 -0500

    added state for input field

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [newItem, setNewItem] = useState("");
+
   return (
     <>
       <form className="new-item-form">
         <div className="form-row">
           <label htmlFor="item">New Item</label>
-          <input type="text" id="item" />
+          <input
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            type="text"
+            id="item"
+          />
         </div>
         <button className="btn">Add</button>
       </form>
@@ -14,7 +22,7 @@ export default function App() {
       <ul className="list">
         <li>
           <label>
-            <input type="checkbox"/>
+            <input type="checkbox" />
             Item 1
           </label>
           <button className="btn btn-danger">Delete</button>
